ae94fe5d1bedb4eef8af9c83b1f3173d35059561
OttoPloch <plochotto@gmail.com>
2026-06-06 21:25:17 -0500

    changed the decoration generation frequency to roughly grow with chunk size.

--- src/config/settings.cpp
+++ src/config/settings.cpp
@@ -16,7 +16,7 @@ void Settings::init()
     chunk_render_distance = renderDistance;
     chunk_load_distance = renderDistance + loadDistanceExtra;
     generation_decoration_scale = -1.f;
-    generation_decoration_frequency = 100;
+    generation_decoration_frequency = chunk_size * 6;
     maxTileZ = 2;
     worldSeed = 117;
 }
